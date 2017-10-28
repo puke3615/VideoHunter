377f839a7771d5ba092b99a1a61504d6ef06e4b1
puke3615 <1129090915@qq.com>
2017-10-28 11:40:55 +0800

    first version

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 import numpy as np
 import imutils
+import time
 import cv2
 
-cap = cv2.VideoCapture('1.mp4')
+cap = cv2.VideoCapture('wolf.mp4')
+cap.set(0, int(4.85e5))
 
-target_W = 192 * 2
-target_H = 108 * 2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+target_W = 1328
 
 while (cap.isOpened()):
     ret, frame = cap.read()
@@ -13,10 +17,26 @@ while (cap.isOpened()):
     h_steps, v_steps = 1, 1
     if isinstance(frame, np.ndarray):
         frame = imutils.resize(frame, width=target_W)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.rectangle(frame, (100, 100), (300, 300), 0xff0000, 2)
+
+        cv2.putText(frame, str(cap.get(0)), (0, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, 0xff0000, 2)
+
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = frame[y:y + h, x:x + w]
+            roi_color = img[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     try:
         cv2.imshow('frame', frame)
+        time.sleep(0.025)
     except Exception as e:
+        print(e)
         break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
---
+++ study.md
@@ -0,0 +1,3 @@
+* 学习OpenCv的[视频处理相关Api](http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html?highlight=videocapture)
+* 
+
